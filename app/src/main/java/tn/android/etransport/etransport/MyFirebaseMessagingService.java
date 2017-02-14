3496911d6be1fa8b3aefa908a55b7d86e1f154fb
mohamed salah bdioui <mohamed.bdioui@utbm.fr>
2017-02-14 10:18:53 +0100

    messaging listener

--- app/src/main/java/tn/android/etransport/etransport/MyFirebaseMessagingService.java
+++ app/src/main/java/tn/android/etransport/etransport/MyFirebaseMessagingService.java
@@ -12,6 +12,11 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import utils.UserInfos;
+
 /**
  * Created by mohamed salah on 12/02/2017.
  */
@@ -22,13 +27,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        //Displaying data in log
-        //It is optional
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
-
-        //Calling method to generate notification
-        sendNotification(remoteMessage.getNotification().getBody());
+        if (remoteMessage.getData().size() > 0) {
+            Log.e(TAG, "Data Payload: " + remoteMessage.getData().toString());
+            try {
+                JSONObject json = new JSONObject(remoteMessage.getData().toString());
+                sendPushNotification(json);
+            } catch (Exception e) {
+                Log.e(TAG, "Exception: " + e.getMessage());
+            }
+        }
     }
 
     //This method is only generating push notification
@@ -52,5 +59,42 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0, notificationBuilder.build());
+    }
+    private void sendPushNotification(JSONObject json) {
+        //optionally we can display the json into log
+        Log.e(TAG, "Notification JSON " + json.toString());
+        try {
+            //getting the json data
+            JSONObject data = json.getJSONObject("data");
+
+            //parsing json data
+            String title = data.getString("title");
+            String message = data.getString("message");
+            String imageUrl = data.getString("image");
+
+            //creating MyNotificationManager object
+            MyNotificationManager mNotificationManager = new MyNotificationManager(getApplicationContext());
+
+            //creating an intent for the notification
+            Intent intent;
+            if (UserInfos.IsConnected)
+                intent = new Intent(getApplicationContext(), Home_activity.class);
+            else
+                intent = new Intent(getApplicationContext(),LoginActivity.class);
+
+            //if there is no image
+            if(imageUrl.equals("null")){
+                //displaying small notification
+                mNotificationManager.showSmallNotification(title, message, intent);
+            }else{
+                //if there is an image
+                //displaying a big notification
+                mNotificationManager.showBigNotification(title, message, imageUrl, intent);
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Json Exception: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
     }
 }
